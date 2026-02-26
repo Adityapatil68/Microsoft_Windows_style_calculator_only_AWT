89134f64578af12c76cbf7481bc403e4f1893251
Adityapatil68 <160546472+Adityapatil68@users.noreply.github.com>
2026-02-26 12:59:23 +0530

    Refactor calculator UI and add keyboard support

--- Microsoft_Windows_style_calculator_only_AWT.java
+++ Microsoft_Windows_style_calculator_only_AWT.java
@@ -1,221 +1,244 @@
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-
 
 import java.awt.*;
-        import java.awt.event.*;
+import java.awt.event.*;
 
-class Microsoft_Windows_style_calculator_only_AWT extends WindowAdapter implements ActionListener {
+class Microsoft_Windows_style_calculator_only_AWT extends WindowAdapter implements ActionListener, KeyListener {
     Frame f;
     TextField TextField1, TextField2;
     Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b0;
     Button badd, bsub, bmult, bdiv, bmod, bcalc, bclr, bpts, bpm, bback, cancle;
     Button bMC, bMR, MemP, MemM, bms, bMem, bfac, bsq, bexp;
 
+    // Logic Variables
+    boolean startNewInput = true;
     double num1, num2, xd, memoryValue = 0;
-    int check;
+    int check = 0;
+
+    Color colBg = new Color(32, 32, 32);
+    Color colBtnNum = new Color(59, 59, 59);
+    Color colBtnOp = new Color(50, 50, 50);
+    Color colBtnEq = new Color(191, 170, 118);
+    Color colText = Color.WHITE;
 
     Microsoft_Windows_style_calculator_only_AWT() {
-        f = new Frame("AWT CALCULATOR");
+        f = new Frame("Calculator");
+        f.setBackground(colBg);
+        f.addKeyListener(this);
 
-        // --- DISPLAYS ---
         TextField2 = new TextField();
         TextField2.setEditable(false);
-        TextField2.setBackground(Color.LIGHT_GRAY);
-        TextField2.setBounds(50, 50, 260, 40);
+        TextField2.setFocusable(false);
+        TextField2.setBackground(colBg);
+        TextField2.setForeground(Color.GRAY);
+        TextField2.setBounds(20, 50, 320, 30);
 
         TextField1 = new TextField("0");
         TextField1.setEditable(false);
-        TextField1.setBackground(Color.WHITE);
-        TextField1.setBounds(50, 100, 260, 60);
-        TextField1.setFont(new Font("Arial", Font.BOLD, 22));
+        TextField1.setFocusable(false);
+        TextField1.setBackground(colBg);
+        TextField1.setForeground(colText);
+        TextField1.setBounds(20, 85, 320, 60);
+        TextField1.setFont(new Font("Segoe UI", Font.BOLD, 48));
 
-        // --- BUTTONS ---
-        bMC = new Button("MC");
-        bMR = new Button("MR");
-        MemP = new Button("M+");
-        MemM = new Button("M-");
-        bms = new Button("MS");
-        bMem = new Button("M");
-        bMC.setBounds(50, 170, 40, 35);
-        bMR.setBounds(95, 170, 40, 35);
-        MemP.setBounds(138, 170, 40, 35);
-        MemM.setBounds(181, 170, 40, 35);
-        bms.setBounds(224, 170, 40, 35);
-        bMem.setBounds(270, 170, 40, 35);
+        // Layout creation (MC, MR, etc.)
+        bMC = createBtn("MC", 20, 155, 45, 30, colBg, colText);
+        bMR = createBtn("MR", 75, 155, 45, 30, colBg, colText);
+        MemP = createBtn("M+", 130, 155, 45, 30, colBg, colText);
+        MemM = createBtn("M-", 185, 155, 45, 30, colBg, colText);
+        bms = createBtn("MS", 240, 155, 45, 30, colBg, colText);
+        bMem = createBtn("M", 295, 155, 45, 30, colBg, colText);
 
-        bmod = new Button("%");
-        bclr = new Button("CE");
-        cancle = new Button("C");
-        bback = new Button("back");
-        bmod.setBounds(50, 215, 55, 50);
-        bclr.setBounds(111, 215, 55, 50);
-        cancle.setBounds(172, 215, 55, 50);
-        bback.setBounds(233, 215, 77, 50);
+        bmod = createBtn("%", 20, 190, 78, 50, colBtnOp, colText);
+        bclr = createBtn("CE", 102, 190, 78, 50, colBtnOp, colText);
+        cancle = createBtn("C", 184, 190, 78, 50, colBtnOp, colText);
+        bback = createBtn("back", 266, 190, 74, 50, colBtnOp, colText);
 
-        bfac = new Button("1/x");
-        bsq = new Button("x^2");
-        bexp = new Button("sqrt");
-        bdiv = new Button("/");
-        bfac.setBounds(50, 270, 55, 50);
-        bsq.setBounds(111, 270, 55, 50);
-        bexp.setBounds(172, 270, 55, 50);
-        bdiv.setBounds(233, 270, 77, 50);
+        bfac = createBtn("1/x", 20, 245, 78, 50, colBtnOp, colText);
+        bsq = createBtn("x^2", 102, 245, 78, 50, colBtnOp, colText);
+        bexp = createBtn("sqrt", 184, 245, 78, 50, colBtnOp, colText);
+        bdiv = createBtn("/", 266, 245, 74, 50, colBtnOp, colText);
 
-        b7 = new Button("7");
-        b8 = new Button("8");
-        b9 = new Button("9");
-        bmult = new Button("*");
-        b7.setBounds(50, 325, 55, 50);
-        b8.setBounds(111, 325, 55, 50);
-        b9.setBounds(172, 325, 55, 50);
-        bmult.setBounds(233, 325, 77, 50);
+        b7 = createBtn("7", 20, 300, 78, 50, colBtnNum, colText);
+        b8 = createBtn("8", 102, 300, 78, 50, colBtnNum, colText);
+        b9 = createBtn("9", 184, 300, 78, 50, colBtnNum, colText);
+        bmult = createBtn("*", 266, 300, 74, 50, colBtnOp, colText);
 
-        b4 = new Button("4");
-        b5 = new Button("5");
-        b6 = new Button("6");
-        bsub = new Button("-");
-        b4.setBounds(50, 380, 55, 50);
-        b5.setBounds(111, 380, 55, 50);
-        b6.setBounds(172, 380, 55, 50);
-        bsub.setBounds(233, 380, 77, 50);
+        b4 = createBtn("4", 20, 355, 78, 50, colBtnNum, colText);
+        b5 = createBtn("5", 102, 355, 78, 50, colBtnNum, colText);
+        b6 = createBtn("6", 184, 355, 78, 50, colBtnNum, colText);
+        bsub = createBtn("-", 266, 355, 74, 50, colBtnOp, colText);
 
-        b1 = new Button("1");
-        b2 = new Button("2");
-        b3 = new Button("3");
-        badd = new Button("+");
-        b1.setBounds(50, 435, 55, 50);
-        b2.setBounds(111, 435, 55, 50);
-        b3.setBounds(172, 435, 55, 50);
-        badd.setBounds(233, 435, 77, 50);
+        b1 = createBtn("1", 20, 410, 78, 50, colBtnNum, colText);
+        b2 = createBtn("2", 102, 410, 78, 50, colBtnNum, colText);
+        b3 = createBtn("3", 184, 410, 78, 50, colBtnNum, colText);
+        badd = createBtn("+", 266, 410, 74, 50, colBtnOp, colText);
 
-        bpm = new Button("+/-");
-        b0 = new Button("0");
-        bpts = new Button(".");
-        bcalc = new Button("=");
-        bpm.setBounds(50, 490, 55, 50);
-        b0.setBounds(111, 490, 55, 50);
-        bpts.setBounds(172, 490, 55, 50);
-        bcalc.setBounds(233, 490, 77, 50);
-
-        Button[] buttons = {b1, b2, b3, b4, b5, b6, b7, b8, b9, b0, bpts, bpm, bback, badd, bsub, bmult, bdiv, bmod, bcalc, cancle, bclr, bMC, bMR, MemP, MemM, bms, bMem, bfac, bsq, bexp};
-        for (Button b : buttons) {
-            b.addActionListener(this);
-            f.add(b);
-        }
+        bpm = createBtn("+/-", 20, 465, 78, 50, colBtnNum, colText);
+        b0 = createBtn("0", 102, 465, 78, 50, colBtnNum, colText);
+        bpts = createBtn(".", 184, 465, 78, 50, colBtnNum, colText);
+        bcalc = createBtn("=", 266, 465, 74, 50, colBtnEq, Color.BLACK);
 
         f.add(TextField1);
         f.add(TextField2);
         f.addWindowListener(this);
-        f.setSize(360, 600);
+        f.setSize(360, 540);
         f.setLayout(null);
+        f.setResizable(false);
         f.setVisible(true);
     }
 
-    // --- YOUR FORMATTING METHOD ---
-    private String formatResult(double result) {
-        if (result == (long) result)
-            return String.valueOf((long) result);
-        else
-            return String.format("%.2f", result)
-                    .replaceAll("0+$", "")
-                    .replaceAll("\\.$", "");
+    private Button createBtn(String label, int x, int y, int w, int h, Color bg, Color fg) {
+        Button b = new Button(label);
+        b.setBounds(x, y, w, h);
+        b.setBackground(bg);
+        b.setForeground(fg);
+        b.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+        b.addActionListener(this);
+        b.addKeyListener(this);
+        f.add(b);
+        return b;
     }
 
-    public void windowClosing(WindowEvent e) {
-        f.dispose();
+    private void handleNumber(String n) {
+        String s = TextField1.getText();
+        if (startNewInput || s.equals("0") || s.equals("Error")) {
+            TextField1.setText(n);
+            startNewInput = false;
+        } else {
+            TextField1.setText(s + n);
+        }
+    }
+
+    private void handleOp(int type, String opLabel) {
+        // If an operator is already active and the user just finished typing a number...
+        if (check != 0 && !startNewInput) {
+            calculateIntermediate();
+        } else {
+            num1 = Double.parseDouble(TextField1.getText());
+        }
+
+        check = type;
+        TextField2.setText(formatResult(num1) + " " + opLabel);
+        startNewInput = true;
+    }
+
+    private void calculateIntermediate() {
+        try {
+            num2 = Double.parseDouble(TextField1.getText());
+            switch (check) {
+                case 1 -> num1 = num1 + num2;
+                case 2 -> num1 = num1 - num2;
+                case 3 -> num1 = num1 * num2;
+                case 4 -> {
+                    if (num2 != 0) num1 = num1 / num2;
+                    else { TextField1.setText("Error"); return; }
+                }
+                case 5 -> num1 = num1 % num2;
+            }
+            TextField1.setText(formatResult(num1));
+        } catch (Exception e) {
+            TextField1.setText("Error");
+        }
+    }
+
+    private void calculate() {
+        if (check == 0) return;
+        calculateIntermediate();
+        TextField2.setText("");
+        check = 0;
+        startNewInput = true;
+    }
+
+    private void handleBackspace() {
+        String s = TextField1.getText();
+        if (s.length() > 0) TextField1.setText(s.length() > 1 ? s.substring(0, s.length()-1) : "0");
+    }
+
+    private void handleClearAll() {
+        TextField1.setText("0"); TextField2.setText(""); num1 = num2 = check = 0; startNewInput = true;
     }
 
     public void actionPerformed(ActionEvent e) {
         Object src = e.getSource();
         String currentStr = TextField1.getText();
 
-        // Number Input
         if (src instanceof Button && "0123456789".contains(((Button) src).getLabel())) {
-            if (currentStr.equals("0") || currentStr.equals("Error")) TextField1.setText(((Button) src).getLabel());
-            else TextField1.setText(currentStr + ((Button) src).getLabel());
+            handleNumber(((Button) src).getLabel());
         }
-
-        // Memory Logic
         else if (src == bMC) {
             memoryValue = 0;
             TextField2.setText("Memory Cleared");
-        } else if (src == bMR) {
+        }
+        else if (src == bMR) {
             TextField1.setText(formatResult(memoryValue));
-        } else if (src == bms) {
-            memoryValue = Double.parseDouble(TextField1.getText());
-            TextField2.setText("Memory Stored: " + formatResult(memoryValue));
-        } else if (src == MemP) {
-            memoryValue += Double.parseDouble(TextField1.getText());
-            TextField2.setText("M: " + formatResult(memoryValue));
-        } else if (src == MemM) {
-            memoryValue -= Double.parseDouble(TextField1.getText());
+            startNewInput = true;
+        }
+        else if (src == bms) {
+            memoryValue = Double.parseDouble(currentStr);
+            TextField2.setText("M Stored: " + formatResult(memoryValue));
+        }
+        else if (src == MemP) {
+            memoryValue += Double.parseDouble(currentStr);
             TextField2.setText("M: " + formatResult(memoryValue));
         }
-
-        // Unary / Scientific Ops
+        else if (src == MemM) {
+            memoryValue -= Double.parseDouble(currentStr);
+            TextField2.setText("M: " + formatResult(memoryValue));
+        }
+        else if (src == bMem) {
+            // Simply show the current memory value in the history line
+            TextField2.setText("Memory Value: " + formatResult(memoryValue));
+        }
         else if (src == bsq) {
             double val = Double.parseDouble(TextField1.getText());
             TextField1.setText(formatResult(val * val));
+            startNewInput = true;
         } else if (src == bexp) {
             TextField1.setText(formatResult(Math.sqrt(Double.parseDouble(TextField1.getText()))));
+            startNewInput = true;
         } else if (src == bfac) {
-            TextField1.setText(formatResult(1 / Double.parseDouble(TextField1.getText())));
+            double val = Double.parseDouble(TextField1.getText());
+            TextField1.setText(val != 0 ? formatResult(1 / val) : "Error");
+            startNewInput = true;
         } else if (src == bpm) {
-            double val = Double.parseDouble(TextField1.getText()) * -1;
-            TextField1.setText(formatResult(val));
+            TextField1.setText(formatResult(Double.parseDouble(TextField1.getText()) * -1));
         } else if (src == bpts && !currentStr.contains(".")) {
             TextField1.setText(currentStr + ".");
+            startNewInput = false;
         }
-
-        // Basic Operators
-        else if (src == badd || src == bsub || src == bmult || src == bdiv || src == bmod) {
-            num1 = Double.parseDouble(TextField1.getText());
-            if (src == badd) check = 1;
-            if (src == bsub) check = 2;
-            if (src == bmult) check = 3;
-            if (src == bdiv) check = 4;
-            if (src == bmod) check = 5;
-            TextField2.setText(formatResult(num1) + " " + ((Button) src).getLabel());
-            TextField1.setText("0");
-        }
-
-        // Equals Logic
-        else if (src == bcalc) {
-            num2 = Double.parseDouble(TextField1.getText());
-            switch (check) {
-                case 1 -> xd = num1 + num2;
-                case 2 -> xd = num1 - num2;
-                case 3 -> xd = num1 * num2;
-                case 4 -> {
-                    if (num2 == 0) {
-                        TextField1.setText("Error");
-                        return;
-                    }
-                    xd = num1 / num2;
-                }
-                case 5 -> xd = num1 % num2;
-            }
-            TextField1.setText(formatResult(xd));
-            TextField2.setText("");
-        }
-
-        // Utility
-        else if (src == cancle) {
-            TextField1.setText("0");
-            TextField2.setText("");
-            num1 = num2 = check = 0;
-        } else if (src == bclr) TextField1.setText("0");
-        else if (src == bback && currentStr.length() > 0) {
-            TextField1.setText(currentStr.length() > 1 ? currentStr.substring(0, currentStr.length() - 1) : "0");
-        }
+        else if (src == badd) handleOp(1, "+");
+        else if (src == bsub) handleOp(2, "-");
+        else if (src == bmult) handleOp(3, "*");
+        else if (src == bdiv) handleOp(4, "/");
+        else if (src == bmod) handleOp(5, "%");
+        else if (src == bcalc) calculate();
+        else if (src == cancle) handleClearAll();
+        else if (src == bclr) TextField1.setText("0");
+        else if (src == bback) handleBackspace();
     }
 
-    public static void main(String args[]) {
-        new Microsoft_Windows_style_calculator_only_AWT();
+    private String formatResult(double result) {
+        if (result == (long) result) return String.valueOf((long) result);
+        return String.format("%.2f", result).replaceAll("0+$", "").replaceAll("\\.$", "");
     }
+
+    // Keyboard support and main method
+    public void keyTyped(KeyEvent e) {
+        char key = e.getKeyChar();
+        if (Character.isDigit(key)) handleNumber(String.valueOf(key));
+        if (key == '.') if(!TextField1.getText().contains(".")) TextField1.setText(TextField1.getText()+".");
+        if (key == '+') handleOp(1, "+");
+        if (key == '-') handleOp(2, "-");
+        if (key == '*') handleOp(3, "*");
+        if (key == '/') handleOp(4, "/");
+        if (key == '%') handleOp(5, "%");
+        if (key == '=' || key == '\n') calculate();
+    }
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) handleBackspace();
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) handleClearAll();
+    }
+    public void keyReleased(KeyEvent e) {}
+    public void windowClosing(WindowEvent e) { f.dispose(); }
+    public static void main(String args[]) { new Microsoft_Windows_style_calculator_only_AWT(); }
 }
